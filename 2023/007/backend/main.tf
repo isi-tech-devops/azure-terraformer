f529d47766ec688d96e122455f11b431e3bf730b
austin <aokojie@gmail.comclear>
2025-06-06 14:56:55 -0400

    up

--- 2023/007/backend/main.tf
+++ 2023/007/backend/main.tf
@@ -1,25 +1,25 @@
 # resource azurerm_resource_group main {
-  name     = "rg-terraform"
-  location = var.location
-}
-resource random_string main {
+# name     = "rg-terraform"
+# location = var.location
+# }
+resource "random_string" "main" {
   # length           = 8
-  upper            = false
-  special          = false
+  upper   = false
+  special = false
 }
 data "azurerm_resource_group" "example" {
   name = "NDDNRCNBISHFXCcDevCorersg"
 }
 
 
-resource azurerm_storage_account main {
+resource "azurerm_storage_account" "main" {
   name                     = "Accumulotrst"
   resource_group_name      = azurerm_resource_group.main.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }
-resource azurerm_storage_container tfstate {
+resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.main.name
   container_access_type = "private"
