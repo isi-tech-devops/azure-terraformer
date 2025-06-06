a246c0272f998d195eeb2b721c21173546a821ea
austin <aokojie@gmail.comclear>
2025-06-06 15:11:06 -0400

    up

--- 2023/007/backend/main.tf
+++ 2023/007/backend/main.tf
@@ -14,7 +14,7 @@ data "azurerm_resource_group" "example" {
 
 resource "azurerm_storage_account" "main" {
   name                     = "Accumulotrst"
-  resource_group_name      = azurerm_resource_group.example.name
+  resource_group_name      = data.azurerm_resource_group.example.name
   location                 = azurerm_resource_group.main.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
